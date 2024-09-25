bbf02531073b22cba023f0a90266ff8ec3a52b3c
dfadeeff <fadejeff@gmail.com>
2024-09-25 19:37:06 +0200

    got new exercises

---
+++ pythonProject/DSA_Crash_Course/StacksQueues/SimplifyPath.py
@@ -0,0 +1,9 @@
+def simplifyPath(path: str) -> str:
+    return 0
+
+
+if __name__ == '__main__':
+    path1 = "/home/"
+    print(simplifyPath(path1))
+    path2 = "/home//foo/"
+    print(simplifyPath(path2))
