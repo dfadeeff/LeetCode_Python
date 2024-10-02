9ecc8499410c53fccff2d14b8c0cdf03cec57ceb
Dmitry Fadeev <ge86yew@mytum.de>
2024-10-02 22:27:39 +0200

    added a few problems

--- pythonProject/DSA_Crash_Course/StacksQueues/SimplifyPath.py
+++ pythonProject/DSA_Crash_Course/StacksQueues/SimplifyPath.py
@@ -1,5 +1,28 @@
+from dns.name import empty
+
+
 def simplifyPath(path: str) -> str:
-    return 0
+    stack = []
+    # chars = [i for i in path]
+    chars = path.split("/")
+
+    for char in chars:
+        if char == "..":
+            if len(stack) > 0:
+                stack.pop()
+        elif char == "." or char == '':
+            continue
+        else:
+            stack.append(char)
+
+    final = ["/" + i for i in stack]
+
+    # print(chars)
+    # print(final)
+    s = "".join(final)
+    if len(s) == 0:
+        s = "/"
+    return s
 
 
 if __name__ == '__main__':
@@ -7,3 +30,9 @@ if __name__ == '__main__':
     print(simplifyPath(path1))
     path2 = "/home//foo/"
     print(simplifyPath(path2))
+    path3 = "/a/b///c/.././d/../f/"
+    print(simplifyPath(path3))
+    path4 = "/home/user/Documents/../Pictures"
+    print(simplifyPath(path4))
+    path5 = "/../"
+    print(simplifyPath(path5))
