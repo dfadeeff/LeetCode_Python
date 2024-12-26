8793df4fae08ce1061020b7c8f1f4a471d73de02
Dmitry Fadeev <ge86yew@mytum.de>
2024-12-26 12:36:35 +0100

    added padded unqiue path solution

--- pythonProject/DSA_Crash_Course/DP/UniquePaths2.py
+++ pythonProject/DSA_Crash_Course/DP/UniquePaths2.py
@@ -6,7 +6,6 @@ class Solution:
         m = len(obstacleGrid)
         n = len(obstacleGrid[0])
 
-
         if obstacleGrid[0][0] == 1:
             return 0
 
@@ -24,26 +23,57 @@ class Solution:
                 obstacleGrid[0][i] = 0
 
         # start from rows
-        for i in range(1, m ):
+        for i in range(1, m):
             # go through cols
-            for j in range(1, n ):
+            for j in range(1, n):
                 if obstacleGrid[i][j] == 0:
                     obstacleGrid[i][j] = obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1]
                 else:
                     obstacleGrid[i][j] = 0
 
-        for i in range(0, m ):
+        for i in range(0, m):
             print(obstacleGrid[i])
-        return obstacleGrid[m-1][n-1]
+        return obstacleGrid[m - 1][n - 1]
+
+    def uniquePathsWithObstaclesPadded(self, obstacleGrid: List[List[int]]) -> int:
+        rows = len(obstacleGrid)
+        cols = len(obstacleGrid[0])
+        dp = [[0] * (cols + 1) for _ in range(rows + 1)]
+
+        if obstacleGrid[0][0] == 1:
+            return 0
+
+        dp[1][1] = 1
+
+        for i in range(0, rows + 1):
+            print(dp[i])
+        print("#########")
+        # start from rows
+        for i in range(1, rows + 1):
+            # go through cols
+            for j in range(1, cols + 1):
+                if i==1 and j==1:
+                    continue
+                if obstacleGrid[i - 1][j - 1] == 0:
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+                else:
+                    dp[i][j] = 0
+
+        for i in range(0, rows + 1):
+            print(dp[i])
+        return dp[rows][cols]
 
 
 def main():
     obstacleGrid = [[0, 0, 0], [0, 1, 0], [0, 0, 0]]
-    print(Solution().uniquePathsWithObstacles(obstacleGrid))
+    # print(Solution().uniquePathsWithObstacles(obstacleGrid))
+    print(Solution().uniquePathsWithObstaclesPadded(obstacleGrid))
     obstacleGrid = [[0, 1], [0, 0]]
-    print(Solution().uniquePathsWithObstacles(obstacleGrid))
+    # print(Solution().uniquePathsWithObstacles(obstacleGrid))
+    print(Solution().uniquePathsWithObstaclesPadded(obstacleGrid))
     obstacleGrid = [[1, 0]]
-    print(Solution().uniquePathsWithObstacles(obstacleGrid))
+    # print(Solution().uniquePathsWithObstacles(obstacleGrid))
+    print(Solution().uniquePathsWithObstaclesPadded(obstacleGrid))
 
 
 if __name__ == '__main__':
