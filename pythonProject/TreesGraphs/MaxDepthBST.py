67a7543d0ef8d1c0b33cb18d3632c6c20b55efd4
Dmitry Fadeev <ge86yew@mytum.de>
2025-02-20 23:09:53 +0100

    added max depth

---
+++ pythonProject/TreesGraphs/MaxDepthBST.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+from TreeNode import TreeNode
+
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        left = self.maxDepth(root.left)
+        right = self.maxDepth(root.right)
+        return max(left, right) + 1
+
+
+if __name__ == '__main__':
+    root = TreeNode(3)
+    root.left = TreeNode(9)
+    root.right = TreeNode(20)
+    root.left.left = TreeNode(15)
+    root.left.right = TreeNode(7)
+    print(Solution().maxDepth(root))
