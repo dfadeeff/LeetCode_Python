4d232c86e82bc14e21566db6152fca96a166fde6
dfadeeff <fadejeff@gmail.com>
2025-02-21 10:23:49 +0100

    pushed new problems

--- pythonProject/TreesGraphs/MaxDepthBST.py
+++ pythonProject/TreesGraphs/MaxDepthBST.py
@@ -12,6 +12,20 @@ class Solution:
         right = self.maxDepth(root.right)
         return max(left, right) + 1
 
+    def maxDepthIterative(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        stack = [(root, 1)]
+        ans = 0
+        while stack:
+            node, depth = stack.pop()
+            ans = max(ans, depth)
+            if node.left:
+                stack.append((node.left, depth + 1))
+            if node.right:
+                stack.append((node.right, depth + 1))
+        return ans
+
 
 if __name__ == '__main__':
     root = TreeNode(3)
@@ -20,3 +34,4 @@ if __name__ == '__main__':
     root.left.left = TreeNode(15)
     root.left.right = TreeNode(7)
     print(Solution().maxDepth(root))
+    print(Solution().maxDepthIterative(root))
