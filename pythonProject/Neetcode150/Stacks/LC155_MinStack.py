f03df46ed27c49cfad79cb61ac0024de29e66bc6
Dmitry Fadeev <fadejeff@gmail.com>
2025-06-22 14:48:46 +0200

    added min stack problem

---
+++ pythonProject/Neetcode150/Stacks/LC155_MinStack.py
@@ -0,0 +1,31 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        val = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
+
+
+if __name__ == "__main__":
+    obj = MinStack()
+    obj.push(-2)
+    obj.push(0)
+    obj.push(-3)
+    print(obj.getMin())
+    print(obj.pop())
+    print(obj.top())
+    print(obj.getMin())
