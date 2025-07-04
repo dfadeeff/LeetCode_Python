dabf66fe86264c5b9f800b68d84645a4fcd2c8ec
Dmitry Fadeev <fadejeff@gmail.com>
2025-07-05 00:52:50 +0200

    added sliding window problems

---
+++ pythonProject/Neetcode150/SlidingWindow/LC658_KClosestElems.py
@@ -0,0 +1,27 @@
+import heapq
+from typing import List
+
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        minheap = []
+        for i in arr:
+            dist = abs(i - x)
+            minheap.append([dist, i])
+        heapq.heapify(minheap)
+        res = []
+        while k > 0:
+            d, el = heapq.heappop(minheap)
+            res.append(el)
+            k -= 1
+        return sorted(res)
+
+
+if __name__ == "__main__":
+    arr = [1, 2, 3, 4, 5]
+    k = 4
+    x = 3
+    print(Solution().findClosestElements(arr, k, x))
+    arr = [1, 1, 2, 3, 4, 5]
+    k = 4
+    x = -1
+    print(Solution().findClosestElements(arr, k, x))
