1b59768540f83fd3d98f5160379e5c7dbe49ba3d
Dmitry Fadeev <fadejeff@gmail.com>
2025-07-06 01:26:09 +0200

    added some comments to max sliding window

--- pythonProject/Neetcode150/SlidingWindow/LC239_SlidingWindowMax.py
+++ pythonProject/Neetcode150/SlidingWindow/LC239_SlidingWindowMax.py
@@ -30,8 +30,10 @@ class Solution:
         q = deque()
         res = []
         for i in range(len(nums)):
-            # Remove elements out of window (left side)
-            if q and q[0] < i- k + 1:
+            # There are a total of n - k + 1 windows, q stores indices, not values, q[0] refers to the index at the front of the deque
+            # Remove elements out of window (left side), i - k + 1 start of the window, left boundary
+
+            if q and q[0] < i - k + 1:
                 q.popleft()
 
             # Remove all smaller elements from the right
@@ -40,8 +42,9 @@ class Solution:
 
             # Add current index
             q.append(i)
-
-            #Store results when window size is reached
+            print("Deque (indices):", q)
+            print("Deque (values):", [nums[i] for i in q])
+            # Store results when window size is reached
             if i >= k - 1:
                 res.append(nums[q[0]])
 
@@ -52,3 +55,4 @@ if __name__ == "__main__":
     nums = [1, 3, -1, -3, 5, 3, 6, 7]
     k = 3
     print(Solution().maxSlidingWindow(nums, k))
+    # print(Solution().maxSlidingWindowNaive(nums, k))
