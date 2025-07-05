ff995a5bfc8f521c56b53153c8f7f46c96f23347
Dmitry Fadeev <fadejeff@gmail.com>
2025-07-06 00:17:06 +0200

    added sliding window

---
+++ pythonProject/Neetcode150/SlidingWindow/LC239_SlidingWindowMax.py
@@ -0,0 +1,54 @@
+from collections import deque
+from typing import List
+
+
+class Solution:
+    def maxSlidingWindowNaive(self, nums: List[int], k: int) -> List[int]:
+        if k > len(nums):
+            return []
+
+        left = 0
+        res = []
+        max_element = float("-inf")
+        window = []
+        for i in range(k):
+            element = nums[i]
+            max_element = max(max_element, element)
+            window.append(element)
+        res.append(max_element)
+
+        for right in range(len(nums) - k):
+            window.append(nums[right + k])
+            window.remove(nums[left])
+            left += 1
+            new_max = max(window)
+            res.append(new_max)
+
+        return res
+
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        q = deque()
+        res = []
+        for i in range(len(nums)):
+            # Remove elements out of window (left side)
+            if q and q[0] < i- k + 1:
+                q.popleft()
+
+            # Remove all smaller elements from the right
+            while q and nums[q[-1]] < nums[i]:
+                q.pop()
+
+            # Add current index
+            q.append(i)
+
+            #Store results when window size is reached
+            if i >= k - 1:
+                res.append(nums[q[0]])
+
+        return res
+
+
+if __name__ == "__main__":
+    nums = [1, 3, -1, -3, 5, 3, 6, 7]
+    k = 3
+    print(Solution().maxSlidingWindow(nums, k))
