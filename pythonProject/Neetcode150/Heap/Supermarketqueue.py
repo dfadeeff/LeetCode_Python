74ed6df9428d20a6785e9aea08fd25d2bfb3b2b3
Dmitry Fadeev <fadejeff@gmail.com>
2025-07-14 20:40:34 +0200

    added descr

--- pythonProject/Neetcode150/Heap/Supermarketqueue.py
+++ pythonProject/Neetcode150/Heap/Supermarketqueue.py
@@ -18,3 +18,14 @@ if __name__ == "__main__":
     customers = [10, 3, 2, 3]
     n = 2
     print(Solution().queue_time(customers, n))
+    customers = [5, 3, 4, 2]
+    n = 2
+
+    """
+    Initial heap: [0, 0]  (2 tills, both free)
+	•	Assign 5 → till with 0 → heap: [0, 5]
+	•	Assign 3 → till with 0 → heap: [5, 3]
+	•	Assign 4 → till with 3 → heap: [5, 7]
+	•	Assign 2 → till with 5 → heap: [7, 7]
+    """
+    print(Solution().queue_time(customers, n))
