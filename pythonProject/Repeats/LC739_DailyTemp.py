06df3d1029148366c29ea83490c2c1863f5f8175
Dmitry Fadeev <fadejeff@gmail.com>
2025-10-05 16:28:56 +0200

    posted monotonis stack

---
+++ pythonProject/Repeats/LC739_DailyTemp.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = []
+        answer = [0] * len(temperatures)
+
+        for i in range(len(temperatures)):
+            while stack and temperatures[stack[-1]] < temperatures[i]:
+                j = stack.pop()
+                answer[j] = i - j
+            stack.append(i)
+        return answer
+
+
+if __name__ == "__main__":
+    temp = [40, 35, 32, 37, 50]
+    print(Solution().dailyTemperatures(temp))
